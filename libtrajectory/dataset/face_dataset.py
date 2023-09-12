6e60f83097b83cb4adaea8a1be3094f5a1a4c92b
YangZhiLiang <7472769+yytennis@user.noreply.gitee.com>
2023-09-12 08:10:28 +0000

    删除文件 libtrajectory

--- libtrajectory/dataset/face_dataset.py
+++
@@ -1,9 +0,0 @@
-from libtrajectory.dataset.abstract_dataset import AbstractDataset
-
-
-class FaceDataset(AbstractDataset):
-    def __init__(self, config):
-        self.config = config
-        self.face_config = self.config.get("face")
-        self.face_mongo = self.face_config.get("mongo")
-
