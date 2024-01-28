699cb64bca8e5d7d583e17dc2f85a7652224ef80
YangZhiLiang <1140852846@qq.com>
2024-01-28 17:47:42 +0800

    node and edge

--- lib-stTrajectory/preprocessing/STEL/preprocessor.py
+++ lib-stTrajectory/preprocessing/STEL/preprocessor.py
@@ -3,21 +3,22 @@ import io
 import numpy as np
 import pandas as pd
 from geopy.distance import geodesic
+
 # from pandarallel import pandarallel
 # pandarallel.initialize(nb_workers=48)
 
 logging.basicConfig(filename='example.log', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-logging.info("-"*50)
+logging.info("-" * 50)
 
 
 class Preprocessor(object):
     def __init__(self, path):
         self.path = path
-        self.data1 = None   # Active
+        self.data1 = None  # Active
         self.data2 = None  # Passive
 
-        self.inter = 60*60*24*7  # 时间分割下的时间间隔阈值
-        self.dis = 1000*1000  # 空间划分下的空间距离阈值 单位m
+        self.inter = 60 * 60 * 24 * 7  # 时间分割下的时间间隔阈值
+        self.dis = 1000 * 1000  # 空间划分下的空间距离阈值 单位m
         self.sthre = 100  # 空间坐标系粒度中空间点个数阈值
         self.tthre = 100  # 时间坐标系粒度中时间点个数阈值
 
@@ -30,12 +31,10 @@ class Preprocessor(object):
         data = pd.read_csv(self.path, dtype={
             'uid': str, 'tid': str, 'time': int, 'lat': float, 'lon': float, 'did': str,
             'm_time': int, 'm_lat': float, 'm_lon': float, 'm_did': str})
-        data1 = data[['tid', 'time', 'lat', 'lon', 'did']]
-        data2 = data[['tid', 'm_time', 'm_lat', 'm_lon', 'm_did']]
-        data2 = data2.rename(columns={'m_time': 'time', 'm_lat': 'lat', 'm_lon': 'lon', 'm_did': 'did'})
-        data2 = data2[data2['time'] != 0]
-        self.data1 = data1
-        self.data2 = data2
+        self.data1 = data[['tid', 'time', 'lat', 'lon', 'did']].copy()
+        self.data2 = data[['tid', 'm_time', 'm_lat', 'm_lon', 'm_did']].copy()
+        self.data2.rename(columns={'m_time': 'time', 'm_lat': 'lat', 'm_lon': 'lon', 'm_did': 'did'}, inplace=True)
+        self.data2 = self.data2[self.data2['time'] != 0]
 
         buffer = io.StringIO()
         self.data1.info(buf=buffer)
@@ -48,8 +47,10 @@ class Preprocessor(object):
 
     def cleaner(self):
         logging.info("data clean...")
-        self.data1 = self.data1.dropna()
-        self.data2 = self.data2.dropna()
+        self.data1.dropna(inplace=True)
+        self.data1.drop_duplicates(inplace=True)
+        self.data2.dropna(inplace=True)
+        self.data2.drop_duplicates(inplace=True)
         logging.info("data clean completed")
 
     def tseg(self):
@@ -254,7 +255,7 @@ class Preprocessor(object):
         raise logging.info(f"current cell is {cell}, cell_t01: {t01}, current point t={t} no find 4split cell")
 
     def ts2vector(self):
-        logging.info("time segment and space coordinate of vector...")
+        logging.info("time segment and space coordinate --> vector...")
         tv0 = np.zeros(self.data1.tseg.max() + 1, dtype=int)  # 初始vector，tseg从0开始
         logging.info(f"time vector length: {len(tv0)}")
         self.data1["tv"] = self.data1['tseg'].map(lambda tseg: self._tseg2v(tseg, tv0))
@@ -271,7 +272,7 @@ class Preprocessor(object):
         buffer = io.StringIO()
         self.data1.info(buf=buffer)
         logging.info(f"data1 info after time segment: {buffer.getvalue()}")
-        logging.info("time segment and space coordinate of vector completed")
+        logging.info("vector completed")
 
     def _tseg2v(self, tseg, tv0):
         tv0[tseg] = 1
@@ -287,13 +288,14 @@ class Preprocessor(object):
         return binary_dict[scoor]
 
     def st2vector(self):
-        logging.info("space segment and time coordinate of vector...")
+        logging.info("space segment and time coordinate --> vector...")
         sseg = self.data2.sseg.unique().tolist()
         latf = max([int(s.split("-")[0]) for s in sseg]) + 1  # 从0开始标记
         lonf = max([int(s.split("-")[1]) for s in sseg]) + 1  # 从0开始标记
         latv0 = np.zeros(latf, dtype=int)  # 初始vector
         lonv0 = np.zeros(lonf, dtype=int)  # 初始vector
-        logging.info(f"lat vector length: {len(latv0)}, lon vector length: {len(lonv0)}, space vector length: {len(latv0)+ len(lonv0)}")
+        logging.info(
+            f"lat vector length: {len(latv0)}, lon vector length: {len(lonv0)}, space vector length: {len(latv0) + len(lonv0)}")
         self.data2["sv"] = self.data2['sseg'].map(lambda sseg: self._sseg2v(sseg, latv0, lonv0))
 
         self.data2['tv'] = self.data2['tcoor'].map(lambda tcoor: ''.join(map(self._coor2v, str(tcoor))))
@@ -302,13 +304,13 @@ class Preprocessor(object):
         self.data2["tv"] = self.data2["tv"].map(lambda tv: tv.ljust(tvlen, '0'))
         self.data2["tv"] = self.data2["tv"].map(lambda tv: np.array([int(i) for i in tv]))  # 转成numpy
 
-        logging.info(f"space time vector length: {len(latv0)+ len(lonv0) + tvlen}")
+        logging.info(f"space time vector length: {len(latv0) + len(lonv0) + tvlen}")
         self.data2["stvec"] = self.data2.apply(lambda row: np.concatenate((row.sv, row.tv)), axis=1)
 
         buffer = io.StringIO()
         self.data2.info(buf=buffer)
         logging.info(f"data2 info after time coordinate: {buffer.getvalue()}")
-        logging.info("space segment and time coordinate of vector completed")
+        logging.info("vector completed")
 
     def _sseg2v(self, sseg, latv0, lonv0):
         latf = int(sseg.split("-")[0])
@@ -317,15 +319,90 @@ class Preprocessor(object):
         lonv0[lonf] = 1
         return np.concatenate((latv0, lonv0))
 
-    def node_edge(self):
-        logging.info("trajectory points as nodes...")
-        dim1 = len(self.data1.tsvec[0])
-        dim2 = len(self.data2.stvec[0])
-        dim = dim1 if dim1 > dim2 else dim2
-        logging.info(f"dimension1={dim1}, dimension2={dim2}, node dimension={dim}")
-        self.data1['node'] = self.data1['tsvec'].map(lambda v: np.pad(v, (0, dim - len(v)), 'constant'))
-        self.data2['node'] = self.data1['stvec'].map(lambda v: np.pad(v, (0, dim - len(v)), 'constant'))
+    # def node_edge(self):
+    #     logging.info("trajectory points as nodes...")
+    #     dim1 = len(self.data1.tsvec[0])
+    #     dim2 = len(self.data2.stvec[0])
+    #     dim = dim1 if dim1 > dim2 else dim2
+    #     logging.info(f"dimension1={dim1}, dimension2={dim2}, node dimension={dim}")
+    #     self.data1['node'] = self.data1['tsvec'].map(lambda v: np.pad(v, (0, dim - len(v)), 'constant'))
+    #     self.data2['node'] = self.data1['stvec'].map(lambda v: np.pad(v, (0, dim - len(v)), 'constant'))
+        
+    def ts2graph(self):
+        logging.info("time segment and space coordinate --> graph...")
+        self.graph1 = self.data1[['tid']].copy()
+        self.graph1['node'] = self.graph1['tsvec']
+        self.graph1.drop_duplicates(inplace=True)
+        self.graph1['node_edge'] = self.graph1['tid'].map(lambda tid: self._ts2ne(tid))
+        self.graph1['node'] = self.graph1['node'].map(lambda g: g['node'])
+        self.graph1['edge_ind'] = self.graph1['edge_ind'].map(lambda g: g['edge_ind'])
+        self.graph1['edge_wei'] = self.graph1['edge_wei'].map(lambda g: g['edge_wei'])
+        self.graph1.drop(columns=['node_edge'], inplace=True)
+        logging.info("time segment and space coordinate graph completed")
 
+    def _ts2ne(self, tid):
+        dft = self.data1.query(f"tid == '{tid}'").copy()
+        dft.sort_values(['time'], inplace=True)
+        dft.reset_index(drop=True, inplace=True)
+        # nodel
+        node = dft.tsvec.tolist()
+        # edge
+        edge_ind = []
+        edge_wei = []
+        dfg = dft.groupby('tseg')
+        for _, sub_df in dfg:
+            if sub_df.shape[0] == 1:
+                break
+            # The spatial distance between two nodes
+            sub_df['node_latlon'] = sub_df.apply(lambda row: {'ind0': row.name, "latlon0": (row.lat, row.lon)}, axis=1)
+            df0 = sub_df[['ind0', 'latlon0']].copy()
+            df0['flag'] = len(df0)
+            df1 = df0.copy()
+            df1.rename(columns={'ind0': 'ind1', 'latlon0': 'latlon1'}, inplace=True)
+            df = df0.merge(df1, how='outer', on='flag')
+            df['edge_ind'] = df.apply(lambda row: [row.ind0, row.ind1], axis=1)
+            edge_ind.append(df.edge_ind.tolist())
+            df['edge_wei'] = df.apply(lambda row: 1 / geodesic(row.latlon0, row.latlon1).m, axis=1)
+            edge_wei.append(df.edge_wei.tolist())
+        return {'node': node, 'edge_ind': edge_ind, 'edge_wei': edge_wei}
+
+    def st2graph(self):
+        logging.info("space segment and time coordinate --> graph")
+        self.graph2 = self.data2[['tid']].copy()  
+        self.graph2['node'] = self.graph2['stvec']
+        self.graph2.drop_duplicates(inplace=True)                                                      
+        self.graph2['node_edge'] = self.graph2['tid'].map(lambda tid: self._st2ne(tid))                 
+        self.graph2['node'] = self.graph2['node'].map(lambda g: g['node'])                              
+        self.graph2['edge_ind'] = self.graph2['edge_ind'].map(lambda g: g['edge_ind'])                  
+        self.graph2['edge_wei'] = self.graph2['edge_wei'].map(lambda g: g['edge_wei'])                  
+        self.graph2.drop(columns=['node_edge'], inplace=True)
+        logging.info("time segment and space coordinate graph completed")
+        
+    def _st2ne(self, tid):                                                                                             
+        dft = self.data2.query(f"tid == '{tid}'").copy()                                                               
+        dft.sort_values(['time'], inplace=True)                                                                        
+        dft.reset_index(drop=True, inplace=True)                                                                       
+        # nodel                                                                                                        
+        node = dft.stvec.tolist()                                                                                      
+        # edge                                                                                                         
+        edge_ind = []                                                                                                  
+        edge_wei = []                                                                                                  
+        dfg = dft.groupby('sseg')                                                                                      
+        for _, sub_df in dfg:                                                                                          
+            if sub_df.shape[0] == 1:                                                                                   
+                break                                                                                                  
+            # The spatial distance between two nodes                                                                   
+            sub_df['node_time'] = sub_df.apply(lambda row: {'ind0': row.name, "time0": row.time}, axis=1)              
+            df0 = sub_df[['ind0', 'time0']].copy()                                                                     
+            df0['flag'] = len(df0)                                                                                     
+            df1 = df0.copy()                                                                                           
+            df1.rename(columns={'ind0': 'ind1', 'time0': 'time1'}, inplace=True)                                       
+            df = df0.merge(df1, how='outer', on='flag')                                                                
+            df['edge_ind'] = df.apply(lambda row: [row.ind0, row.ind1], axis=1)                                        
+            edge_ind.append(df.edge_ind.tolist())                                                                      
+            df['edge_wei'] = df.apply(lambda row: 1 / abs(row.time0 - row.time1), axis=1)                              
+            edge_wei.append(df.edge_wei.tolist())                                                                      
+        return {'node': node, 'edge_ind': edge_ind, 'edge_wei': edge_wei}                                              
 
     def run(self):
         self.loader()
@@ -333,11 +410,13 @@ class Preprocessor(object):
         # Active
         self.tseg()  # time segment
         self.scoor()  # space coordinate
-        self.ts2vector()  # time segment and space coordinate of vector
+        self.ts2vector()  # to vector
+        self.ts2graph()  # to graph
         # Passive
         self.sseg()  # space segment
         self.tcoor()  # time coordinate
-        self.st2vector()  # space segment and time coordinate of vector
+        self.st2vector()  # to vector
+        self.st2graph()  # to graph
 
     def get(self):
         # active、passive
@@ -345,6 +424,6 @@ class Preprocessor(object):
 
 
 if __name__ == "__main__":
-    # preprocessor = Preprocessor("../../dataset/AIS/test10.csv")
-    # preprocessor.run()
+    preprocessor = Preprocessor("../../dataset/AIS/test10.csv")
+    preprocessor.run()
 
